2030fe2fff3c609ce24c6f2bdb56fc9c11d38504
Ronan <ronan.krebel@gmail.com>
2026-02-07 13:33:59 +0100

    First version of auth router and starting with parkig router

--- index.js
+++ index.js
@@ -1,10 +1,32 @@
 import express from "express"
 import cors from "cors"
 import morgan from "morgan"
+import cookieParser from "cookie-parser";
+
 import authRouter from "./routers/authRouter.js"
 const app = express()
 app.use(express.json())
-app.use(cors())
+
+
+app.use((err, req, res, next) => {
+  if (err instanceof SyntaxError && err.status === 400 && 'body' in err) {
+    return res.status(400).json({ 
+      message: "Invalid JSON format"
+    })
+  }
+  next(err)
+})
+
+
+app.use(
+  cors({
+    origin: ["http://localhost:5173"],
+    methods: ["GET", "POST", "DELETE", "PUT", "PATCH", "OPTIONS"],
+    credentials: true,
+  })
+);
+
+app.use(cookieParser());
 app.use(morgan("common"))
 
 app.use("/auth", authRouter)
